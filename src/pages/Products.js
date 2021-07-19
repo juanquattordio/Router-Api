a7200bcba8bd953d2efba56d10589f4fde56fef5
Juan Quattordio <juanquattordio@gmail.com>
2021-07-19 20:54:21 -0300

    Primeras pruebas de renderizado de API de Products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,25 +5,26 @@ function Products() {
 
     useEffect(() => {
         let axios = require("axios");
-        let productsApi = axios("https://fakerapi.it/api/v1/products?_quantity=1&_taxes=12&_categories_type=uuid")
+        let productsApi = axios("https://fakerapi.it/api/v1/products?")
             .then(res => {
                 console.log("OK")
                 setApiProducts(res.data)
-                // console.log(res.data.data)
             })
             .catch(err => {	// catch siempre recibe un error, por si falla la API.
                 console.log("Mensaje Error:")
                 console.log(err.response.statusText)
             })
-
-        // let [apiProducts, setApiProducts] = useState(res.data.data);
-
         console.log(apiProducts)
     }, [])
 
+    let listProducts = apiProducts.data;
 
+    console.log(listProducts)
     return (
-        <h1>{apiProducts.code}</h1>
+        <div>
+            <h1>{apiProducts.code}</h1>
+            {listProducts.map((prod) => <p>{prod.name}</p>)}
+        </div>
     )
 }
 export default Products
