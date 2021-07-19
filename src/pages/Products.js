d69874e77c29990ee69e3ebe53d98d2402e3b3d6
Juan Quattordio <juanquattordio@gmail.com>
2021-07-19 20:32:47 -0300

    Se logra conexion a API de Products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react"
+
 function Products() {
+    let [apiProducts, setApiProducts] = useState("");
+
+    useEffect(() => {
+        let axios = require("axios");
+        let productsApi = axios("https://fakerapi.it/api/v1/products?_quantity=1&_taxes=12&_categories_type=uuid")
+            .then(res => {
+                console.log("OK")
+                setApiProducts(res.data)
+                // console.log(res.data.data)
+            })
+            .catch(err => {	// catch siempre recibe un error, por si falla la API.
+                console.log("Mensaje Error:")
+                console.log(err.response.statusText)
+            })
+
+        // let [apiProducts, setApiProducts] = useState(res.data.data);
+
+        console.log(apiProducts)
+    }, [])
+
+
     return (
-        <h1>Bienvenidos seccion productos</h1>
+        <h1>{apiProducts.code}</h1>
     )
 }
 export default Products
