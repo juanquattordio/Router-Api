a8e38ddf6deb09bdbe93cdec5bd74a4ebd0f965b
Juan Quattordio <juanquattordio@gmail.com>
2021-07-21 17:26:08 -0300

    Progreso en paginación

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,31 +3,36 @@ import axios from "axios";
 import Product from "../components/Product";
 
 function Products(props) {
+
+    let pageSize = 3;
     let [apiProducts, setApiProducts] = useState([]);
     let [apiProductsPartial, setApiProductsPartial] = useState([]);
-    const [limit, setLimit] = useState(3); // defino que tenga 3 productos por pagina
+    const [limit, setLimit] = useState(pageSize); // defino que tenga 3 productos por pagina
     const [initial, setInitial] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
 
+
     const leerApi = async () => {
-        const datos = await fetch(`https://fakerapi.it/api/v1/products?_locale=en_EN&_seed=12456`)
-        const productList = await datos.json()
-        console.log(productList.data)
-        let cantidadPaginas = Math.ceil(productList.total / limit)
-        console.log("cantidadPaginas: " + cantidadPaginas)
-        let productListPartial = apiProducts.slice(initial, limit) // esta es la línea que quiero ejecutar desupés del fetch. Estoy buscando cómo trabajar ocn fetch o axios.
-        setApiProducts(productList.data)
-        setApiProductsPartial(productListPartial)
+        axios.get(`https://fakerapi.it/api/v1/products?_locale=en_EN&_seed=12456`)
+            .then(res => {
+                const productList = res.data;
+                setApiProducts(productList.data);
+                console.log(productList.data)
+                //cantidadPaginas = Math.ceil(productList.total / limit)
+                // console.log("cantidadPaginas: " + cantidadPaginas)
+                let productListPartial = productList.data;
+                // if (currentPage < (cantidadPaginas))
+                productListPartial = productListPartial.slice(initial, limit) // esta es la línea que quiero ejecutar desupés del fetch. Estoy buscando cómo trabajar ocn fetch o axios.
+                console.log(productListPartial)
+                setApiProductsPartial(productListPartial)
+            })
     };
+
+    const cantidadPaginas = Math.ceil(apiProducts.length / pageSize);
+    console.log("cantidadPaginas: " + cantidadPaginas)
 
     useEffect(() => {
         leerApi();
-        console.log("initial: " + initial)
-        console.log("Limit: " + limit)
-        let productListPartial = apiProducts.slice(initial, limit)
-        console.log("Lista parcial ")
-        console.log(productListPartial)
-        setApiProductsPartial(productListPartial)
     }, [initial, limit])
 
     // Probando con props, recibe bien la props pero creo que sigue el problema de async
@@ -40,16 +45,16 @@ function Products(props) {
     // }, [initial, limit])
 
     const nextPage = () => {
-        setInitial(initial + limit)
-        setCurrentPage(currentPage + 1)
-        // if ((cantidadPaginas - currentPage) > 0) {
-        //     setInitial(initial + limit)
-        //     setCurrentPage(currentPage + 1)
-        // } else return
+        if ((cantidadPaginas - currentPage) > 0) {
+            setInitial(initial + limit)
+            setLimit(limit + limit)
+            setCurrentPage(currentPage + 1)
+        } else return
     }
 
     const previousPage = () => {
-        if (currentPage > 1) {
+        alert("previous")
+        if ({ currentPage } > 1) {
             setInitial(initial - limit)
         } else return
     }
@@ -57,11 +62,8 @@ function Products(props) {
     return (
         <div className="container">
             {apiProductsPartial.map((product, index) => <Product key={index} index={index} product={product}></Product>)}
-
-            <div>
-                <div onClick={nextPage}> Previous Page </div>
-                <div onClick={previousPage}> Next Page </div>
-            </div>
+            <div onClick={previousPage}> Previous Page </div>
+            <div onClick={nextPage}> Next Page </div>
         </div>
 
     )
