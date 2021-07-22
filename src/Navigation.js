35ca7b86ca7145b9810c3a903eacd8d11a7f0b5e
Juan Quattordio <juanquattordio@gmail.com>
2021-07-21 21:19:37 -0300

    Se mejora Navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,19 +4,23 @@ function Navigation() {
     return (
         <>
             <Navbar expand="md" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Book-Caffe</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="container justify-content-between">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/books">Books</Nav.Link>
-                        <Nav.Link href="/products">Products</Nav.Link>
-                    </Nav>
-                    <Form className="d-flex ">
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
+                <div className="container">
+                    <Navbar.Brand href="#home">Book-Caffe</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/books">Books</Nav.Link>
+                            <Nav.Link href="/products">Products</Nav.Link>
+                        </Nav>
+                        <Nav className="mr-4">
+                            <Form className="d-flex ">
+                                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
             </Navbar>
         </>
     )
